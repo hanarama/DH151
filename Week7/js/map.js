317ff06e19c82ef3ec46940afb55dddfec0237f6
hanarama <hannahlien@hotmail.com>
2021-05-10 16:24:22 -0700

    lab 7 update

--- Week7/js/map.js
+++ Week7/js/map.js
@@ -4,9 +4,11 @@ let lat = 0;
 let lon = 0;
 let zl = 3;
 let path = '';
-let geojsonPath = 'data/world.json';
+let geojsonPath = 'https://raw.githubusercontent.com/hanarama/DH151/main/Week7/data/world.json';
 let geojson_data;
 let geojson_layer;
+let brew = new classyBrew();
+let fieldtomap;
 
 // initialize
 $( document ).ready(function() {
@@ -33,6 +35,86 @@ function getGeoJSON(){
 		geojson_data = data;
 
 		// call the map function
-		mapGeoJSON()
+		mapGeoJSON('pop_est') // add a field to be used
 	})
 }
+
+// function to map a geojson file
+function mapGeoJSON(){
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle //call a function to style each feature
+	}).addTo(map);
+
+	// fit to bounds
+	map.fitBounds(geojson_layer.getBounds())
+}
+
+function mapGeoJSON(field){
+
+	// clear layers in case it has been mapped already
+	if (geojson_layer){
+		geojson_layer.clearLayers()
+	}
+	
+	// globalize the field to map
+	fieldtomap = field;
+
+	// create an empty array
+	let values = [];
+
+	// based on the provided field, enter each value into the array
+	geojson_data.features.forEach(function(item,index){
+		values.push(item.properties[field])
+	})
+
+	// set up the "brew" options
+	brew.setSeries(values);
+	brew.setNumClasses(5);
+	brew.setColorCode('YlOrRd');
+	brew.classify('quantiles');
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle //call a function to style each feature
+	}).addTo(map);
+
+	map.fitBounds(geojson_layer.getBounds())
+}
+
+function getStyle(feature){
+	return {
+		stroke: true,
+		color: 'white',
+		weight: 1,
+		fill: true,
+		fillColor: brew.getColorInRange(feature.properties[fieldtomap]),
+		fillOpacity: 0.8
+	}
+}
+
+// // style each feature
+// function getStyle(feature){
+// 	return {
+// 		stroke: true,
+// 		color: 'white',
+// 		weight: 1,
+// 		fill: true,
+// 		fillColor: getColor(feature.properties['pop_est']),
+// 		fillOpacity: 0.8
+// 	}
+// }
+
+// // return the color for each feature
+// function getColor(d) {
+
+// 	return d > 1000000000 ? '#800026' :
+// 		   d > 500000000  ? '#BD0026' :
+// 		   d > 200000000  ? '#E31A1C' :
+// 		   d > 100000000  ? '#FC4E2A' :
+// 		   d > 50000000   ? '#FD8D3C' :
+// 		   d > 20000000   ? '#FEB24C' :
+// 		   d > 10000000   ? '#FED976' :
+// 					  '#FFEDA0';
+// }
